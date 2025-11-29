7e06e75a8481cccdfc918da18c37497ee3859698
auaodo <aosinap@gmail.com>
2025-11-29 21:41:12 +0800

    feat: implement local music playback

--- src/app/(home)/music-card.tsx
+++ src/app/(home)/music-card.tsx
@@ -1,30 +1,9 @@
 import Card from '@/components/card'
 import { useCenterStore } from '@/hooks/use-center'
 import { styles as hiCardStyles } from './hi-card'
-import { CARD_SPACING } from '@/consts'
-import { styles as clockCardStyles } from './clock-card'
-import { styles as calendarCardStyles } from './calendar-card'
-import MusicSVG from '@/svgs/music.svg'
-import PlaySVG from '@/svgs/play.svg'
-
-export const styles = {
-	width: 293,
-	height: 66,
-	offset: 120,
-	order: 6
-}
-
-export default function MusicCard() {
-	const center = useCenterStore()
-
-	return (
-		<Card
-			order={styles.order}
-			width={styles.width}
-			height={styles.height}
-			x={center.x + CARD_SPACING + hiCardStyles.width / 2 - styles.offset}
-			y={center.y - clockCardStyles.offset + CARD_SPACING + calendarCardStyles.height + CARD_SPACING}
-			className='flex items-center gap-3'>
+x = { center.x + CARD_SPACING + hiCardStyles.width / 2 - styles.offset }
+y = { center.y - clockCardStyles.offset + CARD_SPACING + calendarCardStyles.height + CARD_SPACING }
+className = 'flex items-center gap-3' >
 			<MusicSVG className='h-8 w-8' />
 
 			<div className='flex-1'>
@@ -38,6 +17,6 @@ export default function MusicCard() {
 			<button className='flex h-10 w-10 items-center justify-center rounded-full bg-white'>
 				<PlaySVG className='text-brand ml-1 h-4 w-4' />
 			</button>
-		</Card>
+		</Card >
 	)
 }
